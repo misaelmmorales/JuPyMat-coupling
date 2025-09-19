a93948c340a4850d4ab4fb91f82fc34c6e93311a
Misael Morales <misaelmiguelmorales@gmail.com>
2025-09-18 23:08:29 -0500

    Add initial project files including README, LICENSE, .gitignore, and demo notebook

---
+++ jupymat.py
@@ -0,0 +1,10 @@
+############################## JuPyMat ##############################
+#### Coupling Python, Julia, and MATLAB for subsurface modeling: ####
+### geomodels, reservoir simulation, and deep learning surrogates ###
+#####################################################################
+###################### Misael M. Morales, 2025 ######################
+#####################################################################
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
